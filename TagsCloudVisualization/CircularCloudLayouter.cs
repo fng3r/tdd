35cb11ef68c9c391d25f871e32050e1684f64090
fng3r <dinvk@mail.ru>
2017-10-31 00:08:25 +0500

    [Test] Layouter should place first rect at center

---
+++ TagsCloudVisualization/CircularCloudLayouter.cs
@@ -0,0 +1,42 @@
+﻿using System.Drawing;
+using NUnit.Framework;
+using FluentAssertions;
+
+namespace TagCloudVisualization
+{
+    class CircularCloudLayouter
+    {
+        private readonly Point center;
+
+        public CircularCloudLayouter(Point center)
+        {
+            this.center = center;
+        }
+
+        public Rectangle PutNextRectangle(Size rectSize)
+        {
+            return Rectangle.Empty;
+        }
+    }
+
+    [TestFixture]
+    public class CircularCloudLayouter_Should
+    {
+        private Point center;
+        private CircularCloudLayouter layouter;
+
+        [SetUp]
+        public void SetUp()
+        {
+            center = new Point(0, 0);
+            layouter = new CircularCloudLayouter(center);
+        }
+
+
+        [Test]
+        public void Place_FirstRectangle_AtCenter()
+        {
+            layouter.PutNextRectangle(new Size(10, 10)).Location.Should().Be(center);
+        }
+    }
+}
