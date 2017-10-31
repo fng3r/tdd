36c0fefaf82add526318d0ba1acaac79cdb934e6
fng3r <dinvk@mail.ru>
2017-10-31 15:59:39 +0500

    Added test for Rectangle.IntersectsWith extension method

--- TagsCloudVisualization/RectangleExtensions.cs
+++ TagsCloudVisualization/RectangleExtensions.cs
@@ -9,7 +9,10 @@ namespace TagsCloudVisualization
 {
     public static class RectangleExtensions
     {
-        public static Point Center(this Rectangle rect)  => new Point(rect.X + rect.Width / 2, rect.Y + rect.Height / 2);
+        public static Point Center(this Rectangle rect)  
+            => new Point(rect.X + rect.Width / 2, rect.Y + rect.Height / 2);
+
+        public static bool IntersectsWith(this Rectangle rect, IEnumerable<Rectangle> otherRects) => false;
     }
 
     [TestFixture]
@@ -19,6 +22,12 @@ namespace TagsCloudVisualization
         public Point Center(Rectangle rect)
         {
             return rect.Center();
+        }
+
+        [TestCaseSource(typeof(RectangleTestData), nameof(RectangleTestData.IntersectsWithTestData))]
+        public bool IntersectsWith(Rectangle rect, IEnumerable<Rectangle> otherRects)
+        {
+            return rect.IntersectsWith(otherRects);
         }
     }
 
@@ -31,6 +40,19 @@ namespace TagsCloudVisualization
                 yield return new TestCaseData(new Rectangle(0, 0, 10, 20)).Returns(new Point(5, 10));
                 yield return new TestCaseData(new Rectangle(0, 100, 50, 50)).Returns(new Point(25, 125));
                 yield return new TestCaseData(new Rectangle(-100, -50, 200, 50)).Returns(new Point(0, -25));
+            }
+        }
+
+        public static IEnumerable IntersectsWithTestData
+        {
+            get
+            {
+                yield return new TestCaseData(
+                        new Rectangle(0, 0, 5, 5), new[] { new Rectangle(10, 10, 1, 1), new Rectangle(15, 15, 1, 1) })
+                    .Returns(false);
+                yield return new TestCaseData(new Rectangle(0, 0, 5, 5), new[] { new Rectangle(0, 0, 5, 5), new Rectangle(100, 100, 5, 5) })
+                    .Returns(true);
+                yield return new TestCaseData(new Rectangle(0, 0, 5, 5), Array.Empty<Rectangle>()).Returns(false);
             }
         }
     }
