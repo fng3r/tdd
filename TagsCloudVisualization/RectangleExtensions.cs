8281dbdcf7b39a9a0e1a6f6c342c17a1192b69dd
fng3r <dinvk@mail.ru>
2017-10-31 19:08:35 +0500

    Move tests to TagsCloudVisualization/Tests
    	modified:   TagsCloudVisualization/CircularCloudLayouter.cs
    	modified:   TagsCloudVisualization/RectangleExtensions.cs
    	copied:     TagsCloudVisualization/CircularCloudLayouter.cs -> TagsCloudVisualization/Tests/CircularCloudLayouter_Should.cs
    	copied:     TagsCloudVisualization/RectangleExtensions.cs -> TagsCloudVisualization/Tests/RectangleExtensionsTests.cs

--- TagsCloudVisualization/RectangleExtensions.cs
+++ TagsCloudVisualization/RectangleExtensions.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
-using NUnit.Framework;
 
 namespace TagsCloudVisualization
 {
@@ -14,47 +11,5 @@ namespace TagsCloudVisualization
 
         public static bool IntersectsWith(this Rectangle rect, IEnumerable<Rectangle> otherRects)
             => otherRects.Any(rect.IntersectsWith);
-    }
-
-    [TestFixture]
-    public class RectangleExtensionsTests
-    {
-        [TestCaseSource(typeof(RectangleTestData), nameof(RectangleTestData.CenterTestData))]
-        public Point Center(Rectangle rect)
-        {
-            return rect.Center();
-        }
-
-        [TestCaseSource(typeof(RectangleTestData), nameof(RectangleTestData.IntersectsWithTestData))]
-        public bool IntersectsWith(Rectangle rect, IEnumerable<Rectangle> otherRects)
-        {
-            return rect.IntersectsWith(otherRects);
-        }
-    }
-
-    public class RectangleTestData
-    {
-        public static IEnumerable CenterTestData
-        {
-            get
-            {
-                yield return new TestCaseData(new Rectangle(0, 0, 10, 20)).Returns(new Point(5, 10));
-                yield return new TestCaseData(new Rectangle(0, 100, 50, 50)).Returns(new Point(25, 125));
-                yield return new TestCaseData(new Rectangle(-100, -50, 200, 50)).Returns(new Point(0, -25));
-            }
-        }
-
-        public static IEnumerable IntersectsWithTestData
-        {
-            get
-            {
-                yield return new TestCaseData(
-                        new Rectangle(0, 0, 5, 5), new[] { new Rectangle(10, 10, 1, 1), new Rectangle(15, 15, 1, 1) })
-                    .Returns(false);
-                yield return new TestCaseData(new Rectangle(0, 0, 5, 5), new[] { new Rectangle(0, 0, 5, 5), new Rectangle(100, 100, 5, 5) })
-                    .Returns(true);
-                yield return new TestCaseData(new Rectangle(0, 0, 5, 5), Array.Empty<Rectangle>()).Returns(false);
-            }
-        }
     }
 }
---
+++ TagsCloudVisualization/Tests/RectangleExtensionsTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using NUnit.Framework;
+
+namespace TagsCloudVisualization
+{
+    [TestFixture]
+    public class RectangleExtensionsTests
+    {
+        [TestCaseSource(typeof(RectangleTestData), nameof(RectangleTestData.CenterTestData))]
+        public Point Center(Rectangle rect)
+        {
+            return rect.Center();
+        }
+
+        [TestCaseSource(typeof(RectangleTestData), nameof(RectangleTestData.IntersectsWithTestData))]
+        public bool IntersectsWith(Rectangle rect, IEnumerable<Rectangle> otherRects)
+        {
+            return rect.IntersectsWith(otherRects);
+        }
+    }
+
+    public class RectangleTestData
+    {
+        public static IEnumerable IntersectsWithTestData
+        {
+            get
+            {
+                yield return new TestCaseData(
+                        new Rectangle(0, 0, 5, 5), new[] { new Rectangle(10, 10, 1, 1), new Rectangle(15, 15, 1, 1) })
+                    .Returns(false);
+                yield return new TestCaseData(new Rectangle(0, 0, 5, 5), new[] { new Rectangle(0, 0, 5, 5), new Rectangle(100, 100, 5, 5) })
+                    .Returns(true);
+                yield return new TestCaseData(new Rectangle(0, 0, 5, 5), Array.Empty<Rectangle>()).Returns(false);
+            }
+        }
+
+        public static IEnumerable CenterTestData
+        {
+            get
+            {
+                yield return new TestCaseData(new Rectangle(0, 0, 10, 20)).Returns(new Point(5, 10));
+                yield return new TestCaseData(new Rectangle(0, 100, 50, 50)).Returns(new Point(25, 125));
+                yield return new TestCaseData(new Rectangle(-100, -50, 200, 50)).Returns(new Point(0, -25));
+            }
+        }
+    }
+}
