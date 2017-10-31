db3ec8794c5b8abf1330f6ad7707dae2d0df6eeb
fng3r <dinvk@mail.ru>
2017-10-31 15:05:38 +0500

    [Test] Rectangle.Center()

--- TagsCloudVisualization/RectangleExtensions.cs
+++ TagsCloudVisualization/RectangleExtensions.cs
@@ -1,12 +1,37 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace TagsCloudVisualization
 {
-    class RectangleExtensions
+    public static class RectangleExtensions
     {
+        public static Point Center(this Rectangle rect) => new Point();
+    }
+
+    [TestFixture]
+    public class RectangleExtensionsTests
+    {
+        [TestCaseSource(typeof(RectangleTestData), nameof(RectangleTestData.CenterTestData))]
+        public Point Center(Rectangle rect)
+        {
+            return rect.Center();
+        }
+    }
+
+    public class RectangleTestData
+    {
+        public static IEnumerable CenterTestData
+        {
+            get
+            {
+                yield return new TestCaseData(new Rectangle(0, 0, 10, 20)).Returns(new Point(5, 10));
+                yield return new TestCaseData(new Rectangle(0, 100, 50, 50)).Returns(new Point(25, 125));
+                yield return new TestCaseData(new Rectangle(-100, -50, 200, 50)).Returns(new Point(0, -25));
+            }
+        }
     }
 }
